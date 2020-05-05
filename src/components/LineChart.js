099820120d9f29fc44297a4c8475abbfe67dc2e4
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-05-04 19:02:13 -0500

    Adds axes

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React from 'react';
 
-export default () => {
+export default ({ data, width, height, precision }) => {
+  const FONT_SIZE = width / 50;
+  const maximumXFromData = Math.max(...data.map((e) => e.x));
+  const maximumYFromData = Math.max(...data.map((e) => e.y));
+
+  const digits =
+    parseFloat(maximumYFromData.toString()).toFixed(precision).length + 1;
+
+  const padding = (FONT_SIZE + digits) * 3;
+  const chartWidth = width - padding * 2;
+  const chartHeight = height - padding * 2;
+
+  const Axis = ({ points }) => (
+    <polyline fill="none" stroke="#ccc" strokeWidth=".5" points={points} />
+  );
+
+  const XAxis = () => (
+    <Axis
+      points={`${padding},${height - padding} ${width - padding},${
+        height - padding
+      }`}
+    />
+  );
+
+  const YAxis = () => (
+    <Axis points={`${padding},${padding} ${padding},${height - padding}`} />
+  );
+
   return (
-    <div>
-      <h1>Line Chart</h1>
-    </div>
+    <svg viewBox={`0 0 ${width} ${height}`}>
+      <XAxis />
+      <YAxis />
+    </svg>
   );
 };
