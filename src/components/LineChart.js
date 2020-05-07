b6aef08bd141f576b4515e8c22c40351466ea59e
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2020-05-06 19:22:45 -0500

    Adds x-axis labels

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -92,9 +92,34 @@ export default ({
     });
   };
 
+  const LabelsXAxis = () => {
+    const y = height - padding + FONT_SIZE * 2;
+
+    return data.map((element, index) => {
+      const x =
+        (element.x / maximumXFromData) * chartWidth + padding - FONT_SIZE / 2;
+      return (
+        <text
+          key={index}
+          x={x}
+          y={y}
+          style={{
+            fill: '#808080',
+            fontSize: FONT_SIZE,
+            fontFamily: 'Helvetica',
+          }}
+        >
+          {element.label}
+        </text>
+      );
+    });
+  };
+
   return (
     <svg viewBox={`0 0 ${width} ${height}`}>
       <XAxis />
+      <LabelsXAxis />
+
       <YAxis />
 
       {numberOfVerticalGuides && <VerticalGuides />}
